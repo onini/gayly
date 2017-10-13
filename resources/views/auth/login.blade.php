a808b520714983201ab724a3c8d330d84e661a1a
leaf <leaf@leaf.local>
2017-10-13 12:52:00 +0800

    menu icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<meta charset="utf-8" />
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>登录 - {{ config('app.name', 'Laravel') }}</title>
+	<title>登录 - {{ config('gayly.name', 'Onini') }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 	<meta content="" name="description" />
 	<meta content="" name="author" />
