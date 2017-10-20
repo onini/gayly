a6defedab0e9279778842c2a720f9e79ffee174f
leaf <leaf@leaf.local>
2017-10-20 11:34:41 +0800

    edit form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,18 +3,12 @@
 	@if ($form->useHeader())
 		<div class="grid-title no-border">
 			<h4>{{ $form->title() }} <span class="semi-bold"></span></h4>
-			<div class="tools">
-				<a href="javascript:;" class="collapse"></a>
-				<a href="#grid-config" data-toggle="modal" class="config"></a>
-				<a href="javascript:;" class="reload"></a>
-				<a href="javascript:;" class="remove"></a>
+			<div class="pull-right">
+				{!! $form->renderHeaderTool() !!}
 			</div>
 		</div>
 	@endif
 	<div class="grid-body no-border">
-        <div class="pull-right">
-            {!! $form->renderHeaderTool() !!}
-        </div>
         @if($form->hasHorizontal())
             {!! $form->open(['class' => "form-horizontal"]) !!}
         @else
